d44f6ff3dab34ffa03b6f81c84e94189fd37130a
Dubster <shevc9@mail.ru>
2024-09-09 22:36:46 +0300

    Entities for authentication were added

---
+++ Library.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,15 @@
+﻿using Library.Domain.Interfaces;
+
+namespace Library.Domain.Entities
+{
+	public class RefreshToken: IEntity
+	{
+		public int  Id { get; set; }
+		public int UserId { get; set; }
+		public string TokenHash { get; set; }
+		public string TokenSalt { get; set; }
+		public DateTime Ts {  get; set; }
+		public DateTime ExpiryDate { get; set; }
+		public User User { get; set; }
+	}
+}
---
+++ Library.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Library.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Entities
+{
+	public class User: IEntity
+	{
+		public int Id { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string PasswordSalt { get; set; }
+		public DateTime Ts {  get; set; }
+		public List<RefreshToken> RefreshTokens { get; set; }
+	}
+}
