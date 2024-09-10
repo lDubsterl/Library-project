a99d1b09c164e6305e95a0dcfadf5f1fac42f0ff
Dubster <shevc9@mail.ru>
2024-09-10 13:13:01 +0300

    Controller classes were added

---
+++ Library.WebAPI/Controllers/ApiBaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Library.WebAPI.Controllers
+{
+	public class ApiBaseController : ControllerBase
+	{
+		protected int UserId => int.Parse(FindClaim(ClaimTypes.NameIdentifier));
+		string? FindClaim(string claimName)
+		{
+			var claimsIdentity = HttpContext.User.Identity as ClaimsIdentity;
+			var claim = claimsIdentity?.FindFirst(claimName);
+			if (claim == null) return null;
+			return claim.Value;
+		}
+	}
+}
---
+++ Library.WebAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.WebAPI.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class AuthenticationController : ApiBaseController
+	{
+
+	}
+}
