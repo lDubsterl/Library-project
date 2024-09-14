594123ba3f229ecc4d06c1dc8fcfaa3cc3736123
Dubster <shevc9@mail.ru>
2024-09-14 19:27:52 +0300

    Validators were added

---
+++ Library.Application/Features/Validators/GenericValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Library.Application.Features.Validators
+{
+	public static class GenericValidator
+	{
+		public static List<string>? ValidateQuery<TQuery>(Type validatorType, TQuery request)
+		{
+			var validator = validatorType.GetConstructor([])?.Invoke(null);
+			var realValidator = validator as AbstractValidator<TQuery>;
+			var result = realValidator?.Validate(request)!;
+			if (result.IsValid)
+				return null;
+			return result.Errors.Select(e => e.ErrorMessage).ToList();
+		}
+	}
+}
---
+++ Library.Application/Features/Validators/PaginationValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Library.Application.DTOs;
+
+namespace Library.Application.Features.Validators
+{
+    public class PaginationValidator : AbstractValidator<PaginationBase>
+    {
+        public PaginationValidator()
+        {
+            RuleFor(e => e.PageNumber).LessThanOrEqualTo(1)
+                .WithMessage("Page number should be at least greater than or equal to 1.");
+            RuleFor(e => e.PageSize).LessThanOrEqualTo(1)
+                .WithMessage("Page size should be at least greater than or equal to 1.");
+        }
+    }
+}
