91fa68163c03b92dd7ad66617a3b8fa4a38a2576
Dubster <shevc9@mail.ru>
2024-09-14 16:27:45 +0300

    Library controller was added

---
+++ Library.WebAPI/Controllers/LibraryController.cs
@@ -0,0 +1,27 @@
+﻿using Library.Application.DTOs;
+using Library.Shared.Results;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.WebAPI.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class LibraryController : ApiBaseController
+	{
+		IMediator _mediator;
+		public LibraryController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		[Authorize]
+		public async Task<Result<List<BookDTO>>> GetUserBooks(int userId)
+		{
+			return await _mediator.Send(userId);
+		}
+	}
+}
